fb6d9d192d09513d699591cf530f91c6129c6c6f
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-01-16 16:52:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Create an object called student with a property called name
+const student = {
+  name: 'John Doe'
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  // Use Object.keys to get an array of all keys in the object
+  return Object.keys(this);
+};
+
+// Test the getKeys() method on the student object
+const keysArray = student.getKeys();
+
+// Log the result
+console.log(keysArray);
